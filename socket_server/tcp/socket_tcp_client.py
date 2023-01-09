8fbdced032f7f452cd887a72775a99743ac6a959
Irina Bystrova <i.bystrova@dreamkas.ru>
2023-01-10 02:01:47 +0300

    Added socket server

---
+++ socket_server/tcp/socket_tcp_client.py
@@ -0,0 +1,8 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', 8888))
+sock.send(b'Test message')
+result = sock.recv(64)
+print('Response: ', result)
+sock.close()
